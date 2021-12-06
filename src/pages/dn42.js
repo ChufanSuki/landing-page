ad2a6706ba933aaab4f763c69dc555f2467750a4
Chufan Chen <50797312+RetownPlato@users.noreply.github.com>
2021-12-07 01:05:25 +0800

    Change index and add dn42 page.

---
+++ src/pages/dn42.js
@@ -0,0 +1,30 @@
+import * as React from "react"
+
+// markup
+const DN42Page = () => {
+  return (
+    <main>
+      <head>
+        <title>DN42</title>
+      </head>
+      <body>
+      <div>
+          <p><strong><span>nl-amst-1</span></strong><span> (Amsterdam, Netherlands)</span></p>
+          <ul>
+            <li><span>ANS:  AS4242422023</span></li>
+            <li><span>Public IPv4: 185.196.220.38</span></li>
+            <li><span>Public IPv4: 185.196.220.38</span></li>
+            <li><span>Public IPv6: 2001:470:1f06:1ad::2</span></li>
+            <li><span>Tunnel IPv4: 172.22.169.224/27</span></li>
+            <li><span>Tunnel IPv6 link-local: fe80:2023</span></li>
+            <li><span>Tunnel IPv6 ULA: fd4e:700e:857::/48</span></li>
+            <li><span>WireGuard Port: &lt;last 5 digits of your ASN&gt;</span></li>
+            <li><span>WireGuard Public Key: Fexv40BO3nCDIPyFytI0TWmN7XqBuLsFR+EItUKDn24=</span></li>
+          </ul>
+        </div>
+      </body>
+    </main>
+  )
+}
+
+export default DN42Page
